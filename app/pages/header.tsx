6bc0f9e231e0f9c6d46e4b175528e7b62565a9b8
AdorablePuddle <45329320+AdorablePuddle@users.noreply.github.com>
2026-02-10 17:28:37 +0700

    Minor: Header color fix

--- app/pages/header.tsx
+++ app/pages/header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
     return (
         <div className = "flex flex-row items-center h-18 justify-evenly border-dotted border-5 border-[#b33c86] bg-[#f5a9b8]">
             <Icon/>
-            <h3 className = "text-[45px] text-shadow-lg/40 font-caveat">A Puddle's Website</h3>
+            <h3 className = "text-[45px] text-white text-shadow-lg/40 font-caveat">A Puddle's Website</h3>
             <Icon/>
         </div>
     )
