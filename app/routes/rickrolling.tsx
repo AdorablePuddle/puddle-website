d0eea23e744617771e2ed5379a95c0d97a8629e6
AdorablePuddle <45329320+AdorablePuddle@users.noreply.github.com>
2026-02-27 10:01:15 +0700

    gullibility

--- app/routes/rickrolling.tsx
+++ app/routes/rickrolling.tsx
@@ -1,12 +1,22 @@
 import { useEffect } from "react";
 
+import Favicon from "react-favicon";
+import webicon from "/icon.png";
+
 export default function Rickrolling() {
     useEffect(() => {
-        window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        document.title = "Secret found!";
+        setTimeout(() => {
+            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        }, 100)
     })
 
     return (
         <div>
+            <Favicon url = {webicon} />
+            <h1>
+                There is Gullible written on the ceiling.
+            </h1>
         </div>
     );
 }
